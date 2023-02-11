f326e2b73925c470feb35706bf378d905a4ccc42
Ty Geri <tygeri@college.harvard.edu>
2023-02-11 11:23:10 -0500

    Final commit, working todo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,10 +15,40 @@ class Todo(db.Model):
     complete = db.Column(db.Boolean)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/")
+def home():
+    todo_list = Todo.query.all()
+    return render_template("base.html", todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
-    db.create_all()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
+
+
+
